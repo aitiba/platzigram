d6c9e79fed85c31038cc8ef3b85c1f2cb24e0734
Aitor Ibañez <freelance@aitoribanez.com>
2016-06-06 18:12:17 +0200

    Más cambios para los idiomas

---
+++ src/translate/en-US.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'likes': '{ likes, plural, ' +
+        '=0 {no likes}' +
+        '=1 {# like}' +
+        'other {# likes}}',
+  'logout': 'Logout',
+  'english': 'English',
+  'spanish': 'Spanish',
+  'signup.subheading': 'Signup to watch your firends pictures\' studing at Platzi',
+  'signup.facebook': 'Signup with Facebook',
+  'signup.text': 'Signup',
+  'email': 'Email',
+  'username': 'Username',
+  'fullname': 'Fullname',
+  'password': 'Password',
+  'signup.call-to-action': 'Signup',
+  'signup.have_account': 'Already have an account?',
+  'signin': 'Signin',
+  'signup.not-have-account': 'Don\'t have an account?',
+  'language': 'Language'
+}
---
+++ src/translate/es.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'likes': '{ likes, number} me gusta',
+  'logout': 'Salir',
+  'english': 'Inglés',
+  'spanish': 'Español',
+  'signup.subheading': 'Regístrate para ver fotos de tus amigos estudiando en Platzi',
+  'signup.facebook': 'Iniciar sesión con Facebook',
+  'signup.text': 'Iniciar sesion',
+  'email': 'Correo electrónico',
+  'username': 'Nombre de usuario',
+  'fullname': 'Nombre completo',
+  'password': 'Contraseña',
+  'signup.call-to-action': 'Regístrate',
+  'signup.have_account': '¿Tienes una cuenta?',
+  'signin': 'Entrar',
+  'signup.not-have-account': '¿No tenes cuenta?',
+  'language': 'Idioma'
+}
