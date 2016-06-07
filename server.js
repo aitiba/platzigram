57eec9a2e24c5f0e3539b04560515492087cfd7c
Aitor Ibañez <freelance@aitoribanez.com>
2016-06-07 18:22:21 +0200

    Subiendo la foto a nuestro servidor web

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 var express = require('express');
+var multer = require('multer');
+var ext = require('file-extension');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now() + '.' + ext(file.originalname))
+  }
+})
+
+var upload = multer({ storage: storage }).single('picture');
 
 var app = express();
 
@@ -48,6 +61,15 @@ app.get('/api/pictures', function(req, res) {
       res.send(pictures);
     }, 2000)
 
+})
+
+app.post('/api/pictures', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.send(500, "Error uploading file");
+    }
+    res.send("File uploaded");
+  })
 })
 
 app.get('/user/platzi', function(req, res) {
